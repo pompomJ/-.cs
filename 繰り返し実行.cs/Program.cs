0242b6b11d7407d3ebaa173c159bba55c068af11
ZamaVerce2 <Verce@LAPTOP-8JD0V3Q5>
2021-11-15 12:56:04 +0900

    プロジェクト ファイルを追加します。

---
+++ 繰り返し実行.cs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace 繰り返し実行.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Enumerable.Range(0, 4);
+
+            foreach (var item in Enumerable.Range(1, 2))
+                Console.WriteLine("I am a cat.");
+        }
+    }
+}
